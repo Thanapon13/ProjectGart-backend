d219ea289a961dcdd2c0f92f3029d765d5344083
Thanathip Chanasri <thanathip.cha@devdeva.tech>
2023-11-19 16:26:35 +0700

    authroute

--- src/app.js
+++ src/app.js
@@ -1,3 +1,36 @@
-const { sequelize } = require("./models");
-sequelize.sync({ force: true });
-// sequelize.sync({ alter: true });
+// const { sequelize } = require("./models");
+// sequelize.sync({ force: true });
+// // sequelize.sync({ alter: true });
+
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const chalk = require("chalk");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
+const notFoundMiddleware = require("./middlewares/not-found");
+const errorMiddleware = require("./middlewares/error");
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(
+  rateLimit({
+    windowMs: 1000 * 60 * 15,
+    max: 1000,
+    message: { message: "to many requests, please try again later" }
+  })
+);
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(chalk.yellowBright.italic.bold`server runnig on port: ${port}`);
+});
